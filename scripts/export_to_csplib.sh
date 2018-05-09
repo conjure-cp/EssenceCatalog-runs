e13c2cafbfcea73b64639fb55d303ccdf5ae67dd
Özgür Akgün <ozgurakgun@gmail.com>
2018-05-09 19:03:47 +0100

    Support multiple problem definitions for a single CSPLib entry

--- scripts/export_to_csplib.sh
+++ scripts/export_to_csplib.sh
@@ -6,12 +6,12 @@ shopt -s nullglob
 
 ROOT_DIR=$(pwd)
 
-rm ${ROOT_DIR}/csplib/Problems/*/models/*.essence
+rm -f ${ROOT_DIR}/csplib/Problems/*/models/*.essence
 
 pushd problems > /dev/null                              # go through all csplib problems in the catalog
 for prob in csplib-*;
 do
-    prob_base=${prob:7}
+    prob_base=${prob:7:7}
     if [ -d "${ROOT_DIR}/csplib/Problems/${prob_base}" ]; then
         cp "${prob}/"*.essence "${ROOT_DIR}/csplib/Problems/${prob_base}/models"
     fi
