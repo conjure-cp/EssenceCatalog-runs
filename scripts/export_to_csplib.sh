1f6248ad30cffde49af66ec74142f14656378ca1
Özgür Akgün <ozgurakgun@gmail.com>
2018-06-01 14:27:39 +1000

    Update the export script to handle -decision dirs

--- scripts/export_to_csplib.sh
+++ scripts/export_to_csplib.sh
@@ -9,11 +9,18 @@ ROOT_DIR=$(pwd)
 rm -f ${ROOT_DIR}/csplib/Problems/*/models/*.essence
 
 pushd problems > /dev/null                              # go through all csplib problems in the catalog
-for prob in csplib-*;
-do
-    prob_base=${prob:7:7}
-    if [ -d "${ROOT_DIR}/csplib/Problems/${prob_base}" ]; then
-        cp "${prob}/"*.essence "${ROOT_DIR}/csplib/Problems/${prob_base}/models"
+for prob in csplib-*; do
+    if [[ $prob != *-decision ]]; then
+        prob_base=${prob:7:7}
+        pushd ${prob} > /dev/null
+        count=$(ls | grep '.essence' | wc -l)
+        # echo $prob $prob_base $count
+        if [ $count != 0 ] ; then
+            if [ -d "${ROOT_DIR}/csplib/Problems/${prob_base}" ]; then
+                cp *.essence "${ROOT_DIR}/csplib/Problems/${prob_base}/models"
+            fi
+        fi
+        popd > /dev/null
     fi
 done
 popd > /dev/null
