e5906adf32f2b9f566ffe7ee24d6fb569c003055
Özgür Akgün <ozgurakgun@gmail.com>
2019-02-15 22:50:39 +0000

    Only estimate the number of models for the more expensive modes

--- scripts/modelling/runConjure.sh
+++ scripts/modelling/runConjure.sh
@@ -14,22 +14,32 @@ mkdir -p "${TARGET_DIR}"
 
 if [ "${CONJURE_MODE}" == "00_compact" ]; then
     FLAGS="-ac --smart-filenames --channelling=no"
+    GENERATE_MODELS=true
 elif [ "${CONJURE_MODE}" == "01_noch_lvlPruned" ]; then
     FLAGS="-ax --smart-filenames --channelling=no --representations-givens=s --representations-auxiliaries=c --representations-quantifieds=c --representations-cuts=c"
+    GENERATE_MODELS=true
 elif [ "${CONJURE_MODE}" == "02_noch_lvlFlat" ]; then
     FLAGS="-ax --smart-filenames --channelling=no --representations-givens=s --representations-auxiliaries=c --representations-quantifieds=c --representations-cuts=c --representation-levels=no"
+    GENERATE_MODELS=true
 elif [ "${CONJURE_MODE}" == "03_full_lvlPruned_paramSparse" ]; then
     FLAGS="-ax --smart-filenames --representation-levels=yes --representations-givens=s"
+    GENERATE_MODELS=false
 elif [ "${CONJURE_MODE}" == "04_full_lvlPruned_paramAll" ]; then
     FLAGS="-ax --smart-filenames --representation-levels=yes --representations-givens=x"
+    GENERATE_MODELS=false
 elif [ "${CONJURE_MODE}" == "05_full_lvlFlat_paramSparse" ]; then
     FLAGS="-ax --smart-filenames --representation-levels=no --representations-givens=s"
+    GENERATE_MODELS=false
 elif [ "${CONJURE_MODE}" == "06_full_lvlFlat_paramAll" ]; then
     FLAGS="-ax --smart-filenames --representation-levels=no --representations-givens=x"
+    GENERATE_MODELS=false
 else
     echo "CONJURE_MODE not recognised: ${CONJURE_MODE}"
     exit 1
 fi
 
-conjure modelling ${FLAGS} ${ESSENCE_FULL} -o ${TARGET_DIR}
+if ${GENERATE_MODELS}; then
+    conjure modelling ${FLAGS} ${ESSENCE_FULL} -o ${TARGET_DIR}
+fi
 
+conjure modelling ${FLAGS} ${ESSENCE_FULL} -o ${TARGET_DIR} --estimate-number-of-models > ${TARGET_DIR}/estimate.txt
