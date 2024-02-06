be4c87650191837c7c069dead90851b1c867076c
seppiabrilla <alessio.pellegrino@studio.unibo.it>
2024-02-06 12:59:16 +0100

    temporarely removed not portfolio options merged with master

--- scripts/modelling/recompute.sh
+++ scripts/modelling/recompute.sh
@@ -4,11 +4,13 @@ set -o nounset
 set -o errexit
 shopt -s nullglob
 
+
 nb_cores=$1
+
 
 mkdir -p logs/versions
 (scutil --get ComputerName || hostname || echo "unknown host") 2> /dev/null | tee logs/versions/computer_name.txt
 conjure --version | tee logs/versions/conjure_version.txt
 
-scripts/modelling/gen_conjure_commands.sh
+scripts/modelling/gen_conjure_commands.sh 
 scripts/modelling/gen_models.sh $nb_cores
