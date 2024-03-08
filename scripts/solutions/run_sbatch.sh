0414d23aa32467607676b91ffa9dbe0e5185ef88
Özgür Akgün <ozgurakgun@gmail.com>
2024-03-08 21:28:28 +0000

    typo

--- scripts/solutions/run_sbatch.sh
+++ scripts/solutions/run_sbatch.sh
@@ -7,6 +7,6 @@ ls -1 | grep '\.sh' > allfiles
 sort -R allfiles -o allfiles
 head -n $nb_tasks_to_submit allfiles > topfiles
 parallel --no-notice -j1 sbatch :::: topfiles
-rm allfiles somefiles
+rm allfiles topfiles
 )
 
