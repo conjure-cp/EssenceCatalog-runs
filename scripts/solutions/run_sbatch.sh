6f3acd03a8611d710e1d0959f72fd0f3a64d9adb
Özgür Akgün <ozgurakgun@gmail.com>
2024-03-11 21:15:10 +0000

    more parallel, more

--- scripts/solutions/run_sbatch.sh
+++ scripts/solutions/run_sbatch.sh
@@ -5,7 +5,7 @@ cd slurm/sh
 ls -1 | grep '\.sh' > allfiles
 if [ -s allfiles ]; then
     # there are some commands to run
-    parallel --no-notice -j1 sbatch :::: allfiles
+    parallel --no-notice sbatch :::: allfiles
     rm allfiles
 
 else
