cf9d6328b20a2b7bd86aaf0f2c48a4b25ae15c40
Özgür Akgün <ozgurakgun@gmail.com>
2024-03-11 19:58:15 +0000

    checkStatsJSON

--- scripts/solutions/checkStatsJSON.py
+++ scripts/solutions/checkStatsJSON.py
@@ -25,30 +25,43 @@ for path in pathlist:
             pass
 
         changed = False
-        if "time out: time limit reached" in sr:
-            if stats["status"] != "TimeOut":
-                print(
-                    f'Found (time limit reached) -- {stats["status"]} -- {path_str}')
-                stats["status"] = "TimeOut"
-                changed = True
-                # print(stats)
+        if "time out: time limit reached" in sr and stats["status"] != "TimeOut":
+            print(
+                f'Found (time limit reached) -- {stats["status"]} -- {path_str}')
+            stats["status"] = "TimeOut"
+            changed = True
 
-        if "undefined identifier" in sr:
-            if stats["status"] != "Error":
-                print(
-                    f'Found (undefined identifier) -- {stats["status"]} -- {path_str}')
-                stats["status"] = "Error"
-                changed = True
-                # print(stats)
-                with open(path_str + ".modified", "w") as f2:
-                    json.dump(stats, f2, indent=2)
+        if "undefined identifier" in sr and stats["status"] != "Error":
+            print(
+                f'Found (undefined identifier) -- {stats["status"]} -- {path_str}')
+            stats["status"] = "Error"
+            changed = True
+
+        if "MiniZinc error: Memory violation detected and error message" in sr and stats["status"] != "Error":
+            print(f'Found (MiniZinc error) -- {stats["status"]} -- {path_str}')
+            stats["status"] = "Error"
+            changed = True
+
+        if "Check failed: ParseFlatzincFile" in sr and stats["status"] != "Error":
+            print(f'Found (MiniZinc error) -- {stats["status"]} -- {path_str}')
+            stats["status"] = "Error"
+            changed = True
+
+        sr = sr.replace("Solver exited with error code:0 and error message",
+                        "IGNORED1")
+        sr = sr.replace("Sub-process exited with error code:0 and error message",
+                        "IGNORED2")
+        sr = sr.replace("SAT solver exited with error code:0 and message",
+                        "IGNORED3")
+        sr = sr.replace("Solver exited with error code:0 and message",
+                        "IGNORED4")
+        sr = sr.replace("Sub-process exited with error code:1 and error message:",
+                        "IGNORED5")
+
+        if "error" in sr and stats["status"] == "OK":
+            print(sr)
 
     if changed:
         with open(path_str, "w") as f2:
             print(f'Saved {path_str}')
             json.dump(stats, f2, indent=2)
-
-    try:
-        os.remove(path_str + ".modified")
-    except FileNotFoundError:
-        pass
