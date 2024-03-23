fd7c8855eb19ce303423c424ed20da523c5e7d2b
Özgür Akgün <ozgurakgun@gmail.com>
2024-03-23 20:24:02 +0000

    more problems

--- scripts/analyse/descriptive-all.sh
+++ scripts/analyse/descriptive-all.sh
@@ -6,5 +6,15 @@ parallel --no-notice \
         csplib-prob006-GolombRuler \
         csplib-prob007-AllIntervalSeries \
         csplib-prob008-VesselLoading \
-	csplib-prob010-SocialGolfers
+	csplib-prob010-SocialGolfers \
+	csplib-prob013-ProgPartyFunction \
+	csplib-prob013-ProgPartyFunctionDecision \
+	csplib-prob013-ProgPartyPartition \
+	csplib-prob015-SchursLemma \
+	csplib-prob017-RamseyNumbers \
+	csplib-prob018-WaterBuckets \
+	csplib-prob019-MagicSquares \
+	csplib-prob021-Crossfigures \
+	csplib-prob022-BusDriverScheduling \
+	csplib-prob023-AbnormalMagicHexagons
 
